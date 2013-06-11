f66c09a8a97c775e0a503cc644d2ac4531a2a2b0
aharter <git@andreas-harter.de>
2013-06-11 22:04:06 +0200

    moved cflags and include definitions
    Change-Id: Iccb4441dd7c3399a9ad8bc06aa08e46ec41eb9bc

--- Android.mk
+++ Android.mk
@@ -17,31 +17,5 @@ $(warning missing stuff)
 USERLAND_BUILD_MMAL := false
 USERLAND_BUILD_HELLOPI := false
 
-USERLAND_CFLAGS := \
-	-D_REENTRANT \
-	-DUSE_VCHIQ_ARM -DVCHI_BULK_ALIGN=1 -DVCHI_BULK_GRANULARITY=1 \
-	-DOMX_SKIP64BIT \
-	-DEGL_SERVER_DISPMANX \
-	-D_LARGEFILE_SOURCE -D_LARGEFILE64_SOURCE \
-	-Wno-multichar \
-	-Wall \
-	-Wno-unused-but-set-variable \
-	-Wno-unused-variable
-
-#	$(LOCAL_PATH)/include \
-
-USERLAND_INCLUDES := \
-	$(LOCAL_PATH) \
-	$(LOCAL_PATH)/host_applications/framework \
-	$(LOCAL_PATH)/host_applications/linux/libs/bcm_host/include \
-	$(LOCAL_PATH)/interface/vcos/pthreads \
-	$(LOCAL_PATH)/interface/vmcs_host/linux \
-	$(LOCAL_PATH)/interface/vmcs_host \
-	$(LOCAL_PATH)/interface/vmcs_host/khronos \
-	$(LOCAL_PATH)/interface/khronos/include \
-	$(LOCAL_PATH)/vchiq_arm \
-	$(LOCAL_PATH)/host_support/include
-
-	LOCAL_C_INCLUDES := $(USERLAND_INCLUDES)
 	include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
